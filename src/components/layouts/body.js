9d8133586255331ac413cff319ed74e705aca143
eliezerbasubi <eliezer.basubi30@gmail.com>
2019-11-06 20:34:59 +0300

    Add projects section

--- src/components/layouts/body.js
+++ src/components/layouts/body.js
@@ -16,6 +16,13 @@ import imgAndroid from '../../assets/images/androiddev.jpg';
 import imgCross from '../../assets/images/cross_platform.jpg';
 
 export default class Body extends Component{
+    state = {
+        images: [imgHtml, imgCss, imgPhp, imgJs, imgNode, imgReact, imgDart, imgFlutter, imgJava],
+        languages: ["html", "css", "php", "javascript", "node", "react", "dart", "flutter","java"],
+        titles: ["HTML5", "CSS", "PHP", "JavaScript", "NodeJs", "ReactJs", "Dart", "Flutter", "Java"],
+        expertise: ["Web Development", "Mobile Development", "Desktop Development", "Graphic Design"],
+        expertise_images: [imgWeb, imgAndroid, imgCross, imgGraphic]
+    }
     render(){
         return (
             <div>
@@ -38,15 +45,9 @@ export default class Body extends Component{
                             <div className="skills">
                             <h1 className="nopadding">I have experience in: </h1>
                                 <div className="skills_cover">
-                                <SkillBox className="html" programming_icon={imgHtml} title="HTML5"/>
-                                <SkillBox className="css" programming_icon={imgCss} title="CSS"/>
-                                <SkillBox className="php" programming_icon={imgPhp} title="Php"/>
-                                <SkillBox className="javascript" programming_icon={imgJs} title="JavaScript"/>
-                                <SkillBox className="node" programming_icon={imgNode} title="NodeJs"/>
-                                <SkillBox className="react" programming_icon={imgReact} title="ReactJs"/>
-                                <SkillBox className="dart" programming_icon={imgDart} title="Dart"/>
-                                <SkillBox className="flutter" programming_icon={imgFlutter} title="Flutter"/>
-                                <SkillBox className="java" programming_icon={imgJava} title="Java"/>
+                                    {this.state.images.map((skill, index) =>
+                                        <SkillBox key={index} className={this.state.languages[index]} programming_icon={skill} title={this.state.titles[index]}/>
+                                    )}
                                 </div>
                             </div>
                         </div>
@@ -58,30 +59,18 @@ export default class Body extends Component{
                     <p>The craft, my routine, my life. Things that I do for living. Things that describe who I am today.</p>
                     <h4>HERE ARE SOME OF MY EXPERTISE</h4>
                     <div className="justify-inline-flex">
-                        <div className="card">
-                            <img alt="" src={imgWeb} className="img_expertise"/>
-                            <h4>Web Development</h4>
-                            <div className="incolor_border"></div>
-                        </div>
-                        <div className="card">
-                            <img alt="" src={imgAndroid} className="img_expertise"/>
-                            <h4>Android Development</h4>
-                            <div className="incolor_border"></div>
-                        </div>
-                        <div className="card">
-                            <img alt="" src={imgCross} className="img_expertise"/>
-                            <h4>Cross-Platform Development</h4>
-                            <div className="incolor_border"></div>
-                        </div>
-                        <div className="card">
-                            <img alt="" src={imgGraphic} className="img_expertise"/>
-                            <h4>Graphic Design</h4>
-                            <div className="incolor_border"></div>
-                        </div>
+                        {this.state.expertise.map((expert, index) => 
+                            <div className="card" key={index}>
+                                <img alt="" src={this.state.expertise_images[index]} className="img_expertise"/>
+                                <h4>{expert}</h4>
+                                <div className="incolor_border"></div>
+                            </div>
+                        )}
                     </div>
                 </section>
 
                 <Available/>
+                <Projects/>
             </div>
         )
     }
@@ -102,6 +91,10 @@ export  class SkillBox extends Component {
 }
 
 export class Available extends Component {
+    state = {
+        score : ["201", "14", "10", 500.000],
+        title: ["CUPS OF COFFEE", "PROJECTS", "CLIENTS", "LINES OF CODE"]
+    }
     render(){
         return (
             <section className="accomplishments">
@@ -113,24 +106,113 @@ export class Available extends Component {
                 </main>
                 <main>
                     <div className="justify-inline-flex fg-color-white">
-                        <div className="sizedbox">
-                            <h2>201</h2>
-                            <p>CUP OF COFFEE</p>
-                        </div>
-                        <div className="sizedbox">
-                            <h2>14</h2>
-                            <p>PROJECTS</p>
-                        </div>
-                        <div className="sizedbox">
-                            <h2>10</h2>
-                            <p>CLIENTS</p>
-                        </div>
-                        <div className="sizedbox">
-                            <h2>500.000</h2>
-                            <p>LINES OF CODE</p>
-                        </div>
+                        {this.state.score.map((archive, index) => 
+                            <div className="sizedbox" key={index}>
+                                <h2>{archive}</h2>
+                                <p>{this.state.title[index]}</p>
+                            </div>
+                        )}
                     </div>
                 </main>
+            </section>
+        );
+    }
+}
+
+export class Projects extends Component{
+    render(){
+        return (
+            <section className="section_project">
+                <h1>WHAT I'VE DEVELOPED</h1>
+                <p>The craft, my routine, my life. Things that I do for living. Things that describe who I am today.</p>
+                <h5>PROJECTS I HAVE COMPLETED</h5>
+                <div className="justify-inline-flex">
+                    <div className="card unshadowed-card">
+                        <img alt="" src={imgWeb} className="img_expertise"/>
+                        <span className="">JavaScript</span>
+                        <span className="dot">Nodejs</span>
+                        {/* <span className="dot">PostgresSQL</span> */}
+                        <h4>WayFarer</h4>
+                        <div className="card-body">
+                            <p>WayFarer is a public bus transportation service.</p>
+                        </div>
+                        <div className="incolor_border"></div>
+                    </div>
+
+                    <div className="card unshadowed-card">
+                        <img alt="" src={imgAndroid} className="img_expertise"/>
+                        <span className="">Android</span>
+                        <span className="dot">Java</span>
+                        <span className="dot">Firebase</span>
+                        <h4>Online Shop</h4>
+                        <div className="card-body">
+                        <p>An android app that allows users to create and manage shops.</p>
+                        </div>
+                        <div className="incolor_border"></div>
+                    </div>
+
+                    <div className="card unshadowed-card">
+                        <img alt="" src={imgCross} className="img_expertise"/>
+                        <span className="">Flutter</span>
+                        <span className="dot">Dart</span>
+                        <span className="dot">Firebase</span>
+                        <h4>Cake Delivery System</h4>
+                        <div className="card-body">
+                        <p>A cross-platform mobile application for delivering cakes in real time.</p>
+                        </div>
+                        <div className="incolor_border"></div>
+                    </div>
+                    <div className="card unshadowed-card">
+                        <img alt="" src={imgGraphic} className="img_expertise"/>
+                        <span className="">PHP</span>
+                        <span className="dot">MySQL</span>
+                        <h4>Student Web Portal</h4>
+                        <div className="card-body">
+                        <p>A web portal application which allows students to view their results. </p>
+                        </div>
+                        <div className="incolor_border"></div>
+                    </div>
+                    <div className="card unshadowed-card">
+                        <img alt="" src={imgWeb} className="img_expertise"/>
+                        <span className="">PHP</span>
+                        <span className="dot">MySQL</span>
+                        <h4>JChat</h4>
+                        <div className="card-body">
+                        <p>A social media app to connect people from different places and different backgrounds. </p>
+                        </div>
+                        <div className="incolor_border"></div>
+                    </div>
+                    <div className="card unshadowed-card">
+                        <img alt="" src={imgAndroid} className="img_expertise"/>
+                        <span className="">Android</span>
+                        <span className="dot">Firebase</span>
+                        <h4>JChat</h4>
+                        <div className="card-body">
+                        <p>A social media app to connect people from different places and different backgrounds. </p>
+                        </div>
+                        <div className="incolor_border"></div>
+                    </div>
+                    <div className="card unshadowed-card">
+                        <img alt="" src={imgCross} className="img_expertise"/>
+                        <span className="">Flutter</span>
+                        <span className="dot">Nodejs</span>
+                        <h4>Fashion</h4>
+                        <div className="card-body">
+                            <p>A cross-platform mobile app for fashion designers to showcase their modals</p>
+                        </div>
+                        <div className="incolor_border"></div>
+                    </div>
+                    <div className="card unshadowed-card">
+                        <img alt="" src={imgGraphic} className="img_expertise"/>
+                        <span className="">PHP</span>
+                        <span className="dot">MySQL</span>
+                        <h4>SelfDoctor</h4>
+                        <div className="card-body">
+                            <p>An android app for treatment under emergent disease circumstances.</p>
+                        </div>
+                        <div className="incolor_border"></div>
+                    </div>
+                </div>
             </section>
         );
     }
